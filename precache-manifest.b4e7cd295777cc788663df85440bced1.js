58b7bb6bd05aa4db61415d614a18df1aafc8ffe3
Volodymyr_Roiuk <heppivova@gmail.com>
2019-11-11 23:43:52 +0300

    Updates

---
+++ precache-manifest.b4e7cd295777cc788663df85440bced1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46aadd047c27dc5705bcc0fc7c3209b",
+    "url": "/redux_ordering-food/index.html"
+  },
+  {
+    "revision": "7259f9423b2bdc94f2ef",
+    "url": "/redux_ordering-food/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "664f134422cb2374a70b",
+    "url": "/redux_ordering-food/static/js/2.b769e8a2.chunk.js"
+  },
+  {
+    "revision": "7259f9423b2bdc94f2ef",
+    "url": "/redux_ordering-food/static/js/main.782684c5.chunk.js"
+  },
+  {
+    "revision": "0dc3ae24071a44f9a397",
+    "url": "/redux_ordering-food/static/js/runtime-main.d5708192.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/redux_ordering-food/static/media/logo.25bf045c.svg"
+  }
+]);
